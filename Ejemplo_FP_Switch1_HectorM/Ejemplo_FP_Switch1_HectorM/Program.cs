ab59a5eaafbc32f8be682d9715f6bf8960686a65
Hectormiguelhernandezcarrillo <115947085+Hectormiguelhernandezcarrillo@users.noreply.github.com>
2022-11-15 22:34:50 -0600

    Add files via upload

---
+++ Ejemplo_FP_Switch1_HectorM/Ejemplo_FP_Switch1_HectorM/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejemplo_FP_Switch1_HectorM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera, categoria;
+            double pagoTotal = 90;
+            byte horasTrabajadas;
+            //----
+            Console.WriteLine("Ingresa el nombre del profesor");
+            nombre = Console.ReadLine();
+            //----
+            Console.WriteLine("Ingresa la carrera a la que perteneces \t\n ISIC \t IGE \t INLG \t IID");
+            carrera = Console.ReadLine();
+            //----
+            Console.WriteLine("Selecciona la categoria del maestro");
+            categoria = Console.ReadLine();
+            //----
+            Console.WriteLine("Ingresa las horas trabajadas del maestro");
+            horasTrabajadas = Convert.ToByte(Console.ReadLine());
+
+            //Switch
+
+            switch (categoria)
+            {
+                case "A":
+                    pagoTotal = (pagoTotal * horasTrabajadas) * 1.0048;
+                    Console.WriteLine(pagoTotal);
+                    break;
+                case "B":
+                    pagoTotal = (pagoTotal * horasTrabajadas) * 1.0063;
+                    Console.WriteLine(pagoTotal);
+                    break;
+                case "C":
+                    pagoTotal = (pagoTotal * horasTrabajadas) * 1.0098;
+                    Console.WriteLine(pagoTotal);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Saliendo del sistema.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Saliendo del sistema..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Saliendo del sistema...");
+            Thread.Sleep(1000);
+        }
+    }
+}
